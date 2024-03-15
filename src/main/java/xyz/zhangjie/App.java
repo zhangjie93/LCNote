b33932c48043a02d6963baa435d2fe15ab021606
zhangjie386 <zhangjie386@jd.com>
2024-03-15 16:22:13 +0800

    add init file

---
+++ src/main/java/xyz/zhangjie/App.java
@@ -0,0 +1,13 @@
+package xyz.zhangjie;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
